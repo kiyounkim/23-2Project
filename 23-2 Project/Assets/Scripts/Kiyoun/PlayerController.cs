a59f0026217b5aa1549af28e119b29ad2155d98d
kiyounkim <21900072@handong.ac.kr>
2023-12-03 19:14:26 +0900

    [Feat/34] Final Edits

--- 23-2 Project/Assets/Scripts/Kiyoun/PlayerController.cs
+++ 23-2 Project/Assets/Scripts/Kiyoun/PlayerController.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-
+    public Text text;
+    public int score = 0;
     private Rigidbody rigid;
     private float moveForce = 7.0f;
     private float horizontal;
@@ -25,5 +28,18 @@ public class PlayerController : MonoBehaviour
         transform.Rotate(0, horizontal, 0);
         //move the player without using rigidbody
         transform.Translate(0, 0, vertical * moveForce * Time.deltaTime);
+        text.text = score.ToString();
+    }
+    void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag == "Enemy" || other.gameObject.tag == "Light"){
+            SceneManager.LoadScene(1);
+        }
+        if(other.gameObject.tag == "Coin"){
+            IncreaseScore();
+            Destroy(other.gameObject);
+        }
+    }
+    public void IncreaseScore(){
+        score++;
     }
 }
